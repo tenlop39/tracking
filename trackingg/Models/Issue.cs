1c9882ba39afb67fc067568b808985fa4c810d35
PavelHavelka <stromoclimber@gmail.com>
2023-03-02 21:02:59 +0100

    zalozena slozka model a Issue.cs, tutorial vube nedavam :D

---
+++ trackingg/Models/Issue.cs
@@ -0,0 +1,27 @@
+﻿namespace trackingg.Models
+{
+    public class Issue
+    {
+        public uint Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public IssueType IssueType { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+    //https://www.youtube.com/watch?v=8LXCxHzEIhc moc tezky tutorial, vubec nechapu. zkoncil jsem na 5:50
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum IssueType
+    {
+        Feature,
+        Bug,
+        Documentation
+    }
+}
